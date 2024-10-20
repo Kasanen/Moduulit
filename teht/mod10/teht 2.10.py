ee7e2d0b3f0444fa595c2a0601875103216d512f
Veijo Kasanen <veijok119@gmail.com>
2024-10-20 20:28:36 +0300

    commit

--- teht/mod10/teht 2.10.py
+++ teht/mod10/teht 2.10.py
@@ -1,30 +1,47 @@
 # 2. Talo
 class Hissi:
-    def __init__(self, alin_kerros = 1, ylin_kerros = 10, kerros = 1):
-        self.alinkerros = alin_kerros
-        self.ylinkerros = ylin_kerros
-        self.kerros = kerros
+    def __init__(self, alinkerros, ylinkerros):
+        self.alinkerros = alinkerros
+        self.ylinkerros = ylinkerros
+        self.kerros = alinkerros
 
-    def kerros_ylos(self, kerros):
-        kerros = kerros + 1
-        return kerros
+    def kerros_ylos(self):
+        if self.kerros < self.ylinkerros:
+            self.kerros += 1
 
-    def kerros_alas(self, kerros):
-        kerros = kerros - 1  
-        return kerros
+    def kerros_alas(self):
+        if self.kerros > self.alinkerros:
+            self.kerros -= 1  
 
     def siirry_kerrokseen(self, kerrokseen):
-        if(kerrokseen < self.ylinkerros):
-            while kerrokseen != self.kerros:
-                if kerrokseen > self.kerros and self.kerros < self.ylinkerros:
-                    self.kerros = self.kerros_ylos(self.kerros)
-                elif kerrokseen < self.kerros and self.kerros > self.alinkerros:
-                    self.kerros = self.kerros_alas(self.kerros)
-                print(self.kerros)
-        else:
+        if kerrokseen < self.alinkerros or kerrokseen > self.ylinkerros:
             print("?")
-        return self.kerros
+            return self.kerros
 
-h = Hissi()
-print(h.kerros)
-h.siirry_kerrokseen(5)
+        while kerrokseen != self.kerros:
+            if kerrokseen > self.kerros:
+                self.kerros_ylos()
+            elif kerrokseen < self.kerros:
+                self.kerros_alas()
+        return self.kerros
+    
+class Talo:
+    def __init__(self, alin_kerros, ylin_kerros, hisseja):
+        self.alinkerros = alin_kerros
+        self.ylinkerros = ylin_kerros
+        self.hisseja = hisseja
+        #Hissien määrittely
+        self.hissit = [Hissi(alin_kerros, ylin_kerros) for i in range(hisseja)]
+
+    def aja_hissia(self, hissi_nro, kerrokseen):
+        self.hissit[hissi_nro].siirry_kerrokseen(kerrokseen)
+        return
+
+t = Talo(1, 10, 3)
+
+t.aja_hissia(1, 4)
+t.aja_hissia(2, 10)
+
+print(t.hissit[0].kerros)
+print(t.hissit[1].kerros)
+print(t.hissit[2].kerros)
